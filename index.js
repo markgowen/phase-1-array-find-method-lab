f46ae6ac6254f0b175efa59d2d37e3435033d2f9
Mark Gowen <mark.gowen78@gmail.com>
2022-11-06 10:36:43 -0800

    Lab solution

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+    let wins = record.find((game) => {
+    return game.result === "W"});
+    if (wins) {
+        return wins.year;
+    } 
+
+}
